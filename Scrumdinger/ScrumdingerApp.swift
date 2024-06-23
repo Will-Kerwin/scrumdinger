c9d5cf091fc31754df9da06969a1c31d75a9d900
Will <williamkerwin@lancsfirerescue.org.uk>
2024-06-23 21:34:49 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Will Kerwin on 23/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
