b2deae11a02dff35c98954bfe8f6ca1dad7db371
Will <williamkerwin@lancsfirerescue.org.uk>
2024-06-26 19:31:30 +0100

    scrum sheet refactor

---
+++ Scrumdinger/Views/EditScrumSheet.swift
@@ -0,0 +1,18 @@
+//
+//  EditViewSheet.swift
+//  Scrumdinger
+//
+//  Created by Will Kerwin on 26/06/2024.
+//
+
+import SwiftUI
+
+struct EditScrumSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditScrumSheet()
+}
